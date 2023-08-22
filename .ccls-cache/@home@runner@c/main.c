02b8d55e11eb3df4c49741a8e8a3c17036e4e5b6
clementebarros <25550553-clementebarros@users.noreply.replit.com>
2023-08-22 20:43:39 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@c/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int multi(int a, int b) { return a * b; }
+
+char info(resultado) {
+  if (resultado > 5) {
+    return printf("mayor que 5");
+  } else {
+    return printf("menor a 5");
+  }
+}
+
+int main(void) {
+  printf("Cargando ...\n....\n");
+  int resultado = multi(5, 1);
+  char ult_resultado = info(resultado);
+  return 0;
+}
